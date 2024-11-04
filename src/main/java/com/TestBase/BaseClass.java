71b88d220a33a125fca8ca66ba0a694b20bf39d4
Aslogeshwaran <logeshwaran.bvn24@gmail.com>
2024-11-04 16:45:05 +0530

    BaseClass Created

---
+++ src/main/java/com/TestBase/BaseClass.java
@@ -0,0 +1,13 @@
+package com.TestBase;
+
+import org.openqa.selenium.WebDriver;
+
+public class BaseClass {
+
+	protected static WebDriver driver;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
